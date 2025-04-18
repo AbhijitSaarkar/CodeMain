529a28d9f8ba3ee9d73ea10baf02709591acaf35
AS1000 <113835909+AbhijitSaarkar@users.noreply.github.com>
2025-04-18 02:05:04 +0000

    update the code

--- Express/Mini project/server.js
+++ Express/Mini project/server.js
@@ -1,9 +1,21 @@
 import express from "express";
 import publicRoutes from "./routes/public.routes.js"
 import privateRoutes from "./routes/private.routes.js"
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
 
 const app = express();
 const PORT = 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if(!fs.existsSync(path.join(__dirname, "logs"))){
+    fs.mkdirSync(path.join(__dirname,"logs"))
+}
 
 // *InBuilt Middleware 
 app.use(express.json());
